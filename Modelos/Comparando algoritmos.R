1bd32bc0a86b5c9703427364030fc44ddacfcbec
Maitane Ostolaza <maitane.ostolaza@alumni.mondragon.edu>
2025-05-14 16:52:43 +0200

    matriz reducida

--- Modelos/Comparando algoritmos.R
+++ Modelos/Comparando algoritmos.R
@@ -48,10 +48,41 @@ df_reducido <- df_reducido_clien[, productos_validos]
 # ----------- COMPROBAMOS REDUCCION
 dim(df_reducido)
 
+# vamos a reducir todavia más la matriz porque tarda mucho
+# Total de productos comprados por cada cliente
+compras_por_cliente_reducido <- rowSums(df_reducido, na.rm = TRUE)
+media_clientes_reducido <- mean(compras_por_cliente_reducido) #175.5105
+max(compras_por_cliente_reducido) # 1706
+min(compras_por_cliente_reducido) # 72
+
+# Total de veces que se compró cada producto (columnas)
+compras_por_producto_reducido <- colSums(df_reducido, na.rm = TRUE)
+media_productos_reducido <- mean(compras_por_producto_reducido) #1409.497
+min(compras_por_producto_reducido) # 1413
+max(compras_por_producto_reducido) #20970
+
+
+# --------------- VUELTA A LA REDUCCION
+clientes_validos2 <- compras_por_cliente_reducido >= 100
+df_reducido_clien2 <- df_reducido [clientes_validos2, ]
+
+# Recalcular compras por producto tras filtrar clientes
+compras_por_producto_filtrado2 <- colSums(df_reducido_clien2, na.rm = TRUE)
+mean(compras_por_producto_filtrado2)
+
+#---------- PRODUCTOS
+productos_validos2 <- compras_por_producto_filtrado2 >= 2000
+df_reducido2 <- df_reducido_clien2[, productos_validos2] 
+
+# comprobamos: 
+dim(df_reducido2) # bien reducido
+
 
 ########################## COMPARACION ALGORITMOS ##############################
 # ahora si, convertimos a matriz
-matriz <- as.matrix(df_reducido)
+matriz <- as.matrix(df_reducido2)
+saveRDS(matriz,"Datos/Resultados/Matriz_red_comp_algos.rds")
+matriz <- readRDS("Datos\\Resultados\\Matriz_red_comp_algos.rds")
 
 # convertimos a realratingmatrix
 storage.mode(matriz) <- "numeric"
@@ -62,8 +93,8 @@ set.seed(8)
 # --------------------------- TRAIN Y TEST
 
 eval_scheme <- evaluationScheme(matriz_rec, method = "split",
-                                train = 0.8, given= 1,
-                                goodRating = 3)
+                                train = 0.8, given= 5,
+                                goodRating = 1)
 
 # -------------------------- ENTRENAR MODELOS :
 algos <- list("random" = list(name = "RANDOM", param = NULL),
@@ -71,28 +102,20 @@ algos <- list("random" = list(name = "RANDOM", param = NULL),
               "UBCF_50nn" = list(name = "UBCF", param = list(nn = 50)),
               "IBCF_Pearson" = list(name = "IBCF", param = list(method = "Pearson")),
               "popular" = list(name = "POPULAR" , param = NULL),
-              "svdf_50" = list(name = "SVDF",param = list(k=50)),
-              "svdf_100" = list(name = "SVDF",param = list(k=100)),
-              "svdf_200" = list(name = "SVDF",param = list(k=200)))
+              "svdf_10" = list(name = "SVDF", param = list(k=10)))
 
 # ----------- TOPNLIST
 eval <- evaluate(eval_scheme, algos, type = "topNList", n = c(1,3,5,10,15,20))
-plot(eval)
+plot(eval) # a priori no hay mucha diferencia entre los algoritmos
 
-
-getConfusionMatrix(eval[["random"]])
-grafico2 <- plot(eval[-c(2, 3)]) 
-grafico4 <- plot(eval,"prec/rec")
-
+# ---- SACAMOS PARAMETROS PARA COMPRAR ALGORITMOS: 
 
 CM_Random <- getConfusionMatrix(eval[["random"]])[[1]]
 CM_UBCF_10 <- getConfusionMatrix(eval[["UBCF_10nn"]])[[1]]
 CM_UBCF_50 <- getConfusionMatrix(eval[["UBCF_50nn"]])[[1]]
 CM_IBCF <- getConfusionMatrix(eval[["IBCF_Pearson"]])[[1]]
 CM_Popular <- getConfusionMatrix(eval[["popular"]])[[1]]
-CM_SVDF_50 <- getConfusionMatrix(eval[["svdf_50"]])[[1]]
-CM_SVDF_100 <- getConfusionMatrix(eval[["svdf_100"]])[[1]]
-CM_SVDF_200 <- getConfusionMatrix(eval[["svdf_200"]])[[1]]
+CM_SVDF_10 <- getConfusionMatrix(eval[["svdf_10"]])[[1]]
 
 # ----------------- RATINGS
 eval_ratings <- evaluate(eval_scheme, algos, type = "ratings", n = c(1,3,5,10,15,20))
@@ -102,9 +125,16 @@ avg(eval_ratings[["UBCF_10nn"]])
 avg(eval_ratings[["UBCF_50nn"]])
 avg(eval_ratings[["IBCF_Pearson"]])
 avg(eval_ratings[["popular"]])
-avg(eval_ratings[["svdf_50"]])
-avg(eval_ratings[["svdf_100"]])
-avg(eval_ratings[["svdf_200"]])
+avg(eval_ratings[["svdf_10"]])
+
+
+################### GRAFICOS PARA LA COMPARACION DE ALGORITMOS: 
+sum(!is.na(matriz_rec)) / (nrow(matriz_rec) * ncol(matriz_rec))
+recs <- predict(Recommender(getData(eval_scheme, "train"), method = "UBCF", param = list(nn = 50)),
+                getData(eval_scheme, "known"), type = "topNList", n = 10)
+
+# Verifica cuántas recomendaciones hay por usuario
+summary(recs)
 
 
 
