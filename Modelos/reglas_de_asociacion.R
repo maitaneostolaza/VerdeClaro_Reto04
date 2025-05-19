8c45f19070b57e5d45544b7bc69cada69c7d8b75
Ganix Guenaga Ormaetxea <ganix.guenaga@alumni.mondragon.edu>
2025-05-19 15:10:51 +0200

    reglas de asociacion

--- Modelos/reglas_de_asociacion.R
+++ Modelos/reglas_de_asociacion.R
@@ -1,11 +1,61 @@
 library(dplyr)
-library(stringr)
-lineas <- readLines("Datos/Resultados/reglas_de_asociacion.txt")
+library(tidyr)
 
-# todas estas son reglas de asociacion con una confianza mayor al 90%
+tickets <- readRDS("Datos\\Transformados\\tickets_limpios.rds")
+productos <- readRDS("Datos\\Originales\\maestroestr.RDS")
+compras<-left_join(tickets, productos, by= "cod_est")
+compras<- compras[, c(2,5)]
+compras <- compras %>%
+  mutate(presente = 1) %>%  # Añadimos una columna para marcar presencia
+  distinct() %>%            # Eliminamos duplicados si existen
+  pivot_wider(
+    names_from = descripcion,
+    values_from = presente,
+    values_fill = list(presente = 0))
+
+
+library(foreign)
+codigos <- gsub("^X", "", colnames(compras))
+
+tabla_nombres <- data.frame(
+  original = colnames(compras),
+  cod_est = codigos
+) %>%
+  left_join(productos, by = "cod_est")
+
+nuevos_nombres <- ifelse(
+  is.na(tabla_nombres$descripcion),
+  tabla_nombres$original,
+  tabla_nombres$descripcion
+)
+
+colnames(compras) <- nuevos_nombres
+
+library(arules)
+transacciones <- as(compras, "transactions")
+
+reglas <- apriori(transacciones, parameter = list(
+  support = 0.1,  # Aparece en al menos 14.678  tickets
+  confidence = 0.8,
+  minlen = 2,  #reglas con al menos 2 items
+  maxlen= 4, #maximo 4 items por regla
+  target = "rules"
+))
+
+
+inspect(head(reglas, 10))
+inspect(reglas)
+
+reglas<- as(reglas, "data.frame")
+
+
+library(dplyr)
+library(stringr)
+
+# todas estas son reglas de asociacion con una confianza mayor al 80%
 
 # extraer solo las reglas (filtrando líneas que contengan "==>")
-reglas <- grep("==>", lineas, value = TRUE)
+reglas <- grep("==>", reglas, value = TRUE)
 
 # Paso 3: extraer y limpiar los componentes de cada regla
 datos <- data.frame(
@@ -21,7 +71,7 @@ datos <- data.frame(
     consecuente = str_trim(str_extract(regla, "(?<===> )(.*?)(?= \\d)")),
     
     # Lista de antecedentes reales (ítems que acaban en =t o =f)
-    antecedentes_lista = str_extract_all(antecedente, ".*?=t|.*?=f")
+    antecedentes_lista = str_extract_all(antecedente, ".?=t|.?=f")
   )
 
 # Expandir los antecedentes a columnas (el maximo de antecedentes es 4)
@@ -45,3 +95,5 @@ reglas$consecuente<-str_remove(reglas$consecuente, "=t")
 reglas$antecedente_1 <- sub("^\\d+\\.\\s+", "", reglas$antecedente_1)
 
 reglas
+
+saveRDS(reglas,file = "Datos/Resultados/Reglas_de_asociacion.rds")
